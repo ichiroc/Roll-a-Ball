41dd6de28454584822e36bf8e044bca7c0ea4501
ichiroc <nobitahouse@gmail.com>
2018-05-07 12:13:09 +0900

    08.障害物とリスタート まで完了

---
+++ Assets/DangerWall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class DangerWall : MonoBehaviour {
+	void OnCollisionEnter(Collision hit){
+		if (hit.gameObject.CompareTag("Player")) {
+			int sceneIndex = EditorSceneManager.GetActiveScene ().buildIndex;
+			EditorSceneManager.LoadScene (sceneIndex);
+		}
+	}
+}
